de2ea540fd14bd7a1e28a5f2d5016408de5b8de5
snmSebastian <snmsebastian@gmail.com>
2024-11-28 15:18:04 -0500

    Forecast v1.1: se añade inf de pais segun codigos de la tabla de power bi

--- sales_data_loader_sharepoint.py
+++ sales_data_loader_sharepoint.py
@@ -1,112 +1,121 @@
 from packages import pd,glob #tratamiento de dato
 
-#paht_data_historica=r'C:\Users\SSN0609\OneDrive - Stanley Black & Decker\Dashboard Marketing\Sales'
-#paht_data_product=r'C:\Users\SSN0609\OneDrive - Stanley Black & Decker\Master Data\Master Data Products Reclasif.xlsx' 
+# Rutas de los archivos
+path_data_historica = r'C:\Users\SSN0609\OneDrive - Stanley Black & Decker\Dashboard Marketing\Sales'
+path_data_product = r'C:\Users\SSN0609\OneDrive - Stanley Black & Decker\Master Data\Master Data Products Reclasif.xlsx'
+path_data_country = r'C:\Users\SSN0609\OneDrive - Stanley Black & Decker\Dashboard Marketing\LAG Report\Codes _ PBI.xlsx'
 
-def sales_sharepoint(paht_data_historica,paht_data_product):
-    
-
+def sales_sharepoint(path_data_historica, path_data_product, path_data_country):
     """ Función para procesar los datos de ventas desde archivos XLSX alojados en SharePoint 
     y cruzarlos con datos maestros de productos.
-
-    Pasos:
-    1. Leer todos los archivos de ventas en la ruta especificada (`paht_data_historica`).
-    2. Unificar los datos de ventas en un único DataFrame (`df_sales`).
-    3. Leer el archivo maestro de productos desde la ruta especificada (`paht_data_product`).
-    4. Seleccionar columnas clave de ventas y productos.
-    5. Cruzar las ventas con los productos para agregar clasificaciones por SKU.
-    6. Agregar una columna de fecha a partir del año fiscal y periodo fiscal.
-    7. Retornar el DataFrame final procesado.
-
-    Args:
-    paht_data_historica (str): Ruta a la carpeta con archivos de ventas en formato XLSX.
-    paht_data_product (str): Ruta al archivo maestro de productos en formato XLSX.
-
-    Returns:
-    pd.DataFrame: DataFrame combinado de ventas y productos.
     """
     # -------------------------------
     # 1. Leer y procesar los archivos de ventas
     # -------------------------------
-    # Lista de archivos en la carpeta que contienen "Sales" en el nombre
-    all_files_sales = glob.glob(paht_data_historica + "/*Sales*.xlsx")
-    
-    #Filtrar archivos que no contengan "path" en el nombre
+    print('Inicializa lectura de xlsx de sales')
+    all_files_sales = glob.glob(path_data_historica + "/*Sales*.xlsx")
     filtered_files_sales = [file for file in all_files_sales if "path" not in file]
 
-    # Leer y unificar los datos de ventas
-    lst_sales=[]
+    lst_sales = []
     for filename in filtered_files_sales:
-        df = pd.read_excel(filename, index_col=None, header=0,dtype=str)
+        df = pd.read_excel(filename, index_col=None, header=0, dtype=str)
         lst_sales.append(df)
     
-    # Concatenar los datos de ventas en un solo DataFrame
-    df_sales= pd.concat(lst_sales,axis=0,ignore_index=True)
+    df_sales = pd.concat(lst_sales, axis=0, ignore_index=True)
+    print('se crea df_sales')
+
+    # ----------------------------------------------------
+    # 2. Leer el archivo maestro de productos - country y fx rate
+    # ----------------------------------------------------
+    print('inicializa lectura archivos xlsx')
+    try:
+         df_products = pd.read_excel(path_data_product, sheet_name='Master Product')
+         df_country = pd.read_excel(path_data_country, sheet_name='Country Code')
+         df_fx_rate = pd.read_excel(path_data_country, sheet_name='FX Rate')
+         print('se cargan los archivos de products, country y fx rate')
+    except Exception as e:
+         print(f"Error al leer los archivos: {e}")
+         return
+    print('se leen los archivos xlsx')
 
     # -------------------------------
-    # 2. Leer el archivo maestro de productos
+    # 3. Cruzar ventas con productos
     # -------------------------------
-    df_products=pd.read_excel(paht_data_product,index_col=None,header=0,dtype=str)
-
-    # -------------------------------
-    # 3. Seleccionar columnas clave
-    # -------------------------------
-
-    Columns_products=['SKU', 'SKU Base', 'SKU Description', 'Brand ', 'GPP Division Code',
-    'GPP Division Reclasif', 'GPP Category Reclasif', 'GPP Portfolio ',
-    'SBU', 'Super SBU', 'GPP SBU', 'GPP SBU Description']
-    df_products=df_products[Columns_products]
-
-    columns_sales=['Fiscal Year', 'Fiscal Period', 'Country Code', 
-    'Sold-To Customer Code','Country Material', 'Total Sales', 'Total Cost', 'Units Sold']
-    df_sales=df_sales[columns_sales]
-
-    # -------------------------------
-    # 4. Cruzar ventas con productos
-    # -------------------------------
-
-    df_sales_and_product=pd.merge(
-    df_sales,
-    df_products,
-    how='left',
-    left_on='Country Material',
-    right_on='SKU'
+    df_sales_and_product = pd.merge(
+        df_sales,
+        df_products,
+        how='left',
+        left_on='Country Material',
+        right_on='SKU',
+        suffixes=('_sales', '_products')
     )
-
+    print("se cruzo sales con products")
     print(f'len(df_sales): {len(df_sales)}')
-    print(f'len(df_salesProdu): {len(df_sales_and_product)}')
-    #-------------------------------
-    #---- Date
-    #-------------------------------
+    print(f'len(df_sales_and_product): {len(df_sales_and_product)}')
+
+    # -------------------------------
+    # 3.1. Crear columna de Date
+    # -------------------------------
     dict_month = {
-    'jan': '01', 'feb': '02', 'mar': '03', 'apr': '04', 'may': '05', 
-    'jun': '06', 'jul': '07', 'aug': '08', 'sep': '09', 'oct': '10', 
-    'nov': '11', 'dec': '12', '1': '01'
+        'jan': '01', 'feb': '02', 'mar': '03', 'apr': '04', 'may': '05', 
+        'jun': '06', 'jul': '07', 'aug': '08', 'sep': '09', 'oct': '10', 
+        'nov': '11', 'dec': '12', '1': '01'
     }
 
-    # Función para convertir año fiscal y periodo fiscal en una fecha
-    def mes(row):
-        month = row['Fiscal Period'].lower()
-        month_num = dict_month[month]
-        year = row['Fiscal Year']
-        date_str = month_num + '-' + year
-        return date_str
+    df_sales_and_product['Date'] = pd.to_datetime(
+        df_sales_and_product['Fiscal Year'] + '-' + 
+        df_sales_and_product['Fiscal Period'].str.lower().map(dict_month) + '-01',
+        format='%Y-%m-%d'
+    )
+    print('se crea Date')
+    # -----------------------------------------
+    # 4. Cruzar sales_products con Country
+    # ----------------------------------------
+    df_sales_and_product['key_country'] = df_sales_and_product['Country Code'] + df_sales_and_product['Destination Country']
 
-    # Aplicar la función para crear la columna 'Date'
-    df_sales_and_product['Date']=df_sales_and_product.apply(mes,axis=1) 
-    df_sales_and_product['Date']=pd.to_datetime(df_sales_and_product['Date'],format='%Y-%m-%d')
-    #df_SalesAndProduct.head()
+    df_sales_and_product = pd.merge(
+       df_sales_and_product,
+        df_country,
+        how='left',
+        left_on='key_country',
+        right_on='Country Code Concat',
+        suffixes=('_sales_products', '_country')
+    )
+    print("se cruzo sales_products con country")
 
+        # -------------------------------
+    # 4.1. Seleccionar columnas clave
     # -------------------------------
-    # 6. Limpieza final
+    df_sales_and_product.columns = df_sales_and_product.columns.str.strip()
+
+
+
+    colum_sales=[
+        # products
+        'SKU', 'SKU Base', 'SKU Description', 'Brand', 'GPP Division Code',
+        'GPP Division Reclasif', 'GPP Category Reclasif', 'GPP Portfolio',
+        'SBU', 'Super SBU', 'GPP SBU', 'GPP SBU Description', 'Brand + SBU', 'Corded / Cordless', 'Category Group',
+        # sales
+        'Date', 'Fiscal Year', 'Fiscal Period', 'Sold-To Customer Code',
+        'Total Sales', 'Total Cost', 'Units Sold',
+        #Country
+        'Country', 'Region'
+
+    ]
+    df_sales_and_product=df_sales_and_product[colum_sales]  
+    print("se seleccionan columnas")
     # -------------------------------
-    for col in df_sales_and_product.columns:
-        df_sales_and_product.rename(columns={col: col.strip()}, inplace=True)
-    
-    df_sales_and_product = df_sales_and_product.sort_values(by=['Date', 'Country Code', 'Brand'], ascending=[True, False, False])
-    df_sales_and_product['Total Sales']=pd.to_numeric(df_sales_and_product['Total Sales'])
-    df_sales_and_product=df_sales_and_product[df_sales_and_product['Total Sales']>0]
+    # . Limpieza final
+    # -------------------------------
+    df_sales_and_product = df_sales_and_product.sort_values(by=['Date', 'Country', 'Brand'], ascending=[True, False, False])
+    df_sales_and_product['Total Sales'] = pd.to_numeric(df_sales_and_product['Total Sales'])
+    df_sales_and_product = df_sales_and_product[df_sales_and_product['Total Sales'] > 0]
+
+    ruta_csv = r'C:\Users\SSN0609\OneDrive - Stanley Black & Decker\Dashboards LAG\Proyects\Forecast\sales_historical.csv'
+    df_sales_and_product.to_csv(ruta_csv, index=False)
+
     print("se ejecuto correctamente sales_sharepoint")
     print("-------------------------------------------------------------------------------\n")
 
-    return df_sales_and_product
+sales_sharepoint(path_data_historica, path_data_product, path_data_country)
+print('fin ok')
